53d38d29e23da627789025bcd8f6df7d56c73388
Petch <petchkubb@live.com>
2020-07-28 09:19:52 +0700

    integrate on explore screen

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,14 +1,67 @@
 import React from 'react';
-import {View, StyleSheet, Text, SafeAreaView} from 'react-native';
+import {View, StyleSheet, Text, SafeAreaView,FlatList,ScrollView} from 'react-native';
 import Header from '../components/Header';
+import Card from '../components/Card';
+import {useSelector} from 'react-redux' 
+
+
+const LitleCard = ({name}) => {
+  return (
+    <View style={styles.titleCardContainer}>
+      <Text style={styles.titleText}>{name}</Text>
+    </View>
+  )
+}
 
 const Explore = () => {
+  const cardData = useSelector(state=>{
+    return state
+  })
   return (
     <SafeAreaView style={{flex: 1}}>
       <Header />
-      <Text>Explore</Text>
+      <ScrollView>
+        <View style={{flexDirection:'row',flexWrap:'wrap',justifyContent:'space-around'}}>
+          <LitleCard name="Gaming"/>
+          <LitleCard name="Sport"/>
+          <LitleCard name="Music"/>
+          <LitleCard name="Trending"/>
+          <LitleCard name="Movies"/>
+          <LitleCard name="Fashion"/>
+        </View>
+        <View style={{borderBottomWidth:1,marginHorizontal:8}}>
+          <Text style={{margin:8,fontSize:22,borderBottomWidth:1}}>Trending Vedios</Text>
+        </View>
+        <FlatList
+          data={cardData}
+          keyExtractor={(item,index)=>String(index)}
+          renderItem={({item})=>{
+            return <Card
+              vedioId={item.id.videoId}
+              title={item.snippet.title}
+              channel={item.snippet.channelTitle}
+            />
+          }}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  titleCardContainer:{
+    backgroundColor:'red',
+    width:180,
+    height:50,
+    borderRadius:4,
+    marginTop:10,
+    justifyContent:'center'
+  },
+  titleText:{
+    textAlign:'center',
+    color:'white',
+    fontSize:22
+  }
+})
 
 export default Explore;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {  SafeAreaView,FlatList } from 'react-native';
 import {useSelector} from 'react-redux' 
 import Header from '../components/Header';
